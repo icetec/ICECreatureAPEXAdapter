d90af2fdf7c184396a23be9feeaaf035f06245d1
icetec <pit@ice-technologies.de>
2016-06-13 18:32:28 +0200

    Init

---
+++ Scripts/Editor/ICECreatureAPEXAdapterEditor.cs
@@ -0,0 +1,39 @@
+﻿// ##############################################################################
+//
+// ICECreatureAPEXAdapterEditor.cs
+// Version 1.1.21
+//
+// © Pit Vetterick, ICE Technologies Consulting LTD. All Rights Reserved.
+// http://www.ice-technologies.com
+// mailto:support@ice-technologies.com
+// 
+// Unity Asset Store End User License Agreement (EULA)
+// http://unity3d.com/legal/as_terms
+//
+// ##############################################################################
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+
+using ICE;
+using ICE.World;
+using ICE.World.EditorUtilities;
+using ICE.World.EditorInfos;
+
+namespace ICE.Creatures.Adapter
+{
+	[CustomEditor(typeof(ICECreatureAPEXAdapter))]
+	public class ICECreatureAPEXAdapterEditor : ICEWorldBehaviourEditor {
+
+		public override void OnInspectorGUI()
+		{
+			ICECreatureAPEXAdapter _target = DrawDefaultHeader<ICECreatureAPEXAdapter>();
+
+			DrawDefaultFooter( _target );
+		}
+	}
+}
---
+++ Scripts/ICECreatureAPEXAdapter.cs
@@ -0,0 +1,97 @@
+﻿// ##############################################################################
+//
+// ICECreatureAPEXAdapter.cs
+// Version 1.1.21
+//
+// © Pit Vetterick, ICE Technologies Consulting LTD. All Rights Reserved.
+// http://www.ice-technologies.com
+// mailto:support@ice-technologies.com
+// 
+// Unity Asset Store End User License Agreement (EULA)
+// http://unity3d.com/legal/as_terms
+//
+// The ICECreatureAstarAdapter Script based on the default movement script AIPath.cs
+// which comes with the A* Pathfinding Project and provides ICECreatureControl to 
+// use the powerful navigation of the A* Pathfinding System.
+//
+// ##############################################################################
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using ICE.World;
+
+using ICE.Creatures;
+using ICE.Creatures.Objects;
+
+using Apex.Steering.Components;
+
+namespace ICE.Creatures.Adapter
+{
+	[RequireComponent(typeof(SteerForPathComponent))]
+	[RequireComponent(typeof(ICECreatureControl))]
+	public class ICECreatureAPEXAdapter : ICEWorldBehaviour {
+
+		private ICECreatureControl m_Controller = null;
+		private SteerForPathComponent m_SteerForPathComponent = null;
+
+		protected Transform m_Transform = null;
+	
+		// Use this for initialization
+		public override void Awake () {
+
+			//just to optimize the transform component lookup
+			m_Transform = transform;		
+
+
+			m_Controller = GetComponent<ICECreatureControl>();
+			if( m_Controller != null )
+			{
+				m_Controller.Creature.Move.OnTargetMovePositionReached += OnTargetMovePositionReached;
+				m_Controller.Creature.Move.OnMoveComplete += OnMoveComplete;
+				m_Controller.Creature.Move.OnUpdateMovePosition += OnMoveUpdatePosition;
+			}
+
+			m_SteerForPathComponent = GetComponent<SteerForPathComponent>();
+
+		}
+		// Use this for initialization
+		public override void Start () {
+		
+
+		}
+
+		public void OnDisable () 
+		{
+			m_Controller.Creature.Move.OnTargetMovePositionReached -= OnTargetMovePositionReached;
+			m_Controller.Creature.Move.OnMoveComplete -= OnMoveComplete;
+			m_Controller.Creature.Move.OnUpdateMovePosition -= OnMoveUpdatePosition;
+		}
+		
+		// Update is called once per frame
+		public override void Update () {
+		
+		}
+
+		void OnMoveComplete( GameObject _sender, TargetObject _target  )
+		{
+
+		}
+
+		private Vector3 _current_position = Vector3.zero;
+
+		void OnMoveUpdatePosition(  GameObject _sender, Vector3 _origin_position, ref Vector3 _new_position )
+		{
+			if( m_SteerForPathComponent && _current_position != _new_position )
+			{
+				_current_position = _new_position;
+				m_SteerForPathComponent.MoveTo( _new_position, false );
+			}
+		}
+
+		void OnTargetMovePositionReached( GameObject _sender, TargetObject _target )
+		{
+		}
+	}
+}
